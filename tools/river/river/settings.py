c93519417ba6a1c9cf0775ee693c229ecfbccb75
zhao <zztemp001@gmail.com>
2012-10-18 18:33:48 +0800

    update

--- tools/river/river/settings.py
+++ tools/river/river/settings.py
@@ -2,10 +2,30 @@
 
 BOT_NAME = 'chrome'
 BOT_VERSION = '21.0'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
+ROBOTSTXT_OBEY = False  # 是否遵守网站的robots.txt文件规则
+DNSCACHE_ENABLED = True  # 开启DNS缓存
+DEFAULT_RESPONSE_ENCODING = 'utf-8'  # 当文件中没有编码时使用的编码类型，仅用于TextResponse对象
+
+DEPTH_LIMIT = 3  # 爬行深度
+DOWNLOAD_DELAY = 1.25  # 下载间隔时间，以秒为单位，如果此项设置为0，则不会有任何间隔
+RANDOMIZE_DOWNLOAD_DELAY = True  # 与DOWNLOAD_DELAY共同作用，实际值为DOWNLOAD_DELAY*(0.5~1.5)
+DOWNLOAD_TIMEOUT = 180  # 下载超时设定，以秒为单位
+
+CONCURRENT_ITEMS = 100  # 使用pipeline同时处理的item数量
+CONCURRENT_REQUESTS = 16  # 最大下载线程数
+CONCURRENT_REQUESTS_PER_DOMAIN = 8  # 针对单一域名启动的线程数
+CONCURRENT_REQUESTS_PER_IP = 0  # 针对单一IP启动的线程数，如果为0，则使用域名数，如果非0，则覆盖域名设置
 
 SPIDER_MODULES = ['river.spiders']
 NEWSPIDER_MODULE = 'river.spiders'
-USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
+# 缺省的Http请求头信息
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+}
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.contrib.downloadermiddleware.httpproxy.HttpProxyMiddleware': 110,
@@ -16,35 +36,16 @@ ITEM_PIPELINES = [
     # 'river.pipelines.DBPipeline',
 ]
 
-# 使用Proxy：第一步，在settings文件中定义代理列表
-PROXIES = [
-    '112.25.12.36:80',
-    '220.195.192.172:80',
-    '58.53.192.218:8123',
-    '122.72.80.99:80',
-    '122.72.80.105:80',
-    '203.66.187.252:80',
-    '218.247.129.3:80',
-    '112.25.12.38:80',
-    '202.142.17.120:80',
-    '61.55.141.10:80',
-    '122.72.80.106:80',
-    '122.72.80.107:80',
-    '112.25.12.39:80',
-    '112.25.12.37:80',
-    '122.72.80.100:80',
-    '121.33.249.170:8080',
-]
-"""
-PROXIES = [
-        {'ip_port': 'xx.xx.xx.xx:xxxx', 'user_pass': 'foo:bar'},
-        {'ip_port': 'PROXY2_IP:PORT_NUMBER', 'user_pass': 'username:password'},
-        {'ip_port': 'PROXY3_IP:PORT_NUMBER', 'user_pass': ''},
-]
-"""
-
+# log服务设置
 LOG_ENABLED = False
 LOG_ENCODING = 'utf-8'
-LOG_FILE = './log/local.log'
+LOG_FILE = 'river\\log\\local.log'
 LOG_LEVEL = 'DEBUG'
-LOG_STDOUT = False  #是否将屏幕显示的信息一并log进文件，缺省为False
+LOG_STDOUT = False  #是否将屏幕显示的信息一并log进文件，缺省为False。如果为True，则屏幕不再显示信息
+
+# 信息记录设置
+STATS_ENABLED = True
+STATS_DUMP = True  # 在log文件中记录stat信息
+DOWNLOADER_STATS = True
+DEPTH_STATS = True  # 是否收集爬行深度信息
+DEPTH_STATS_VERBOSE = False  # 是否记录每一深度的request数量
--- tools/river/river/spiders/baseinfo.py
+++ tools/river/river/spiders/baseinfo.py
@@ -7,25 +7,89 @@ from os import path
 from time import time
 
 from scrapy.spider import BaseSpider
+from scrapy.http import Request, Response
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
 from scrapy.item import Item
 from scrapy import log
 
-from river.items import PlaceItem
+class DianpingSpider(CrawlSpider):
+    ''' 大众点评网数据抓取
+    1、获取所有市级以上的首页网址：
+    2、在城市首页网址中进入分类页面：
+    3、根据分类页面的商户类型二级子类别+行政区二级子类别，生成列表页首页网址。如（福田区）车公庙片区+（美食）粤菜
+        列表页首页网址格式为：
+    4、根据列表页首页网址，爬行所有列表页面，得到所有商铺的名称、网址、所在城市、区域、商圈，所在的分类、子分类
+    5、根据商铺地址，开展详细数据的抓取，以及后期的数据更新
+    6、可以通过每个分类的“rss订阅”来监控新商户的添加
+    '''
+    name = 'dianping'
+    allowed_domain = ['www.dianping.com', '127.0.0.1']
+    start_urls = ['http://127.0.0.1/static/page/dianping.htm']
+    base_url = 'http://www.dianping.com'
 
-# 抓取大洲/洲片区/国家
-# 目标网址：http://baike.baidu.com/view/4262975.htm
+    def parse(self, response):
+        ''' 从城市列表页中得到城市首页网址，并将其送入请求序列
+        1、城市分热门和非热门，热门城市用粗体表示
+        2、parse函数是spider类缺省用于处理start_urls的方法
+        '''
+        print u'正在 %s 中获取城市列表及其网址' % response.url
+
+        hxs = HtmlXPathSelector(response)
+        cities = hxs.select('//*[@id="divPY"]/li/div/a')
+        for city in cities[:5]:  # 使用前面5条记录测试
+            if city.select('strong'):
+                city_name = city.select('strong/text()').extract()[0]
+                hot = 1
+            else:
+                city_name = city.select('text()').extract()[0]
+                hot = 0
+            city_url = city.select('@href').extract()[0]
+            print str(hot) + "  " + city_name + "  " + city_url
+            # 以下示范如何生成Request，以及如何使用Request.meta来在不同的页面间传递附加变量
+            request = Request(self.base_url + city_url, callback=self.parse_city_home_url,)
+            request.meta['city'] = city_name
+            request.meta['hot'] = hot
+            yield request
+
+    def parse_city_home_url(self, response):
+        ''' 获取城市分类首页地址，重点城市与一般城市有所区别
+        城市总体分类页格式：/shopall/(城市编号)/0
+        '''
+        print u'进入 %s 中' % response.url
+        city_home_url = ''
+        hxs = HtmlXPathSelector(response)
+        if response.meta['hot']:
+            # 重点城市直接得到首页地址(/shopall/342/0)
+            city_home_url = hxs.select('//*[@id="top"]/div[3]/div[1]/div/div[2]/div/a/@href').extract()[0]
+        else:
+            # 非重点城市通过获取美食栏目地址(/search/category/255/10)，得到该城市的编号，然后硬编码首页地址
+            temp_url = hxs.select('//*[@id="top"]/div[3]/div[2]/div/ul[1]/li/ul/li[2]/ul/li[1]/a/@href').extract()[0]
+            city_no = temp_url.split('/')[-2]
+            city_home_url = u'/shopall/%s/0' % city_no
+        print response.meta['city'] + u'首页网址：' + self.base_url + city_home_url
+        return Item()
+
+    def parse_hot_city_list_url(self, response):
+        ''' 通过城市总体分类页，获取分类列表的首页地址
+        重点城市（类别的二级分类×区域的二级分类，如[福田区]车公庙片区+[美食]粤菜）：http://www.dianping.com/search/category/7/10/g407r1951o10p1
+        非重点城市（类别的一级分类×区域的一级分类，如邯山区+美食）：
+        注意要使用o参数进行排序，确保质量好的商铺排在前面，o10为“按点评数”排序，确保有人气
+        注意可能存在没有数据的组合，网站会提示“没有找到满足条件的商户。”
+        '''
+        pass
+
 class WorldPlaceSpider(BaseSpider):
+    ''' 抓取大洲/洲片区/国家
+    目标网址：http://baike.baidu.com/view/4262975.htm
+    '''
     name = 'worldplace'
     allowed_domains = ['localhost', '127.0.0.1']
     start_urls = ['http://127.0.0.1/static/page/4262975.htm',]
     filename = 'river\\data\\basic.sqlite'
 
-
     def parse(self, response):
-        item = PlaceItem()
         hxs = HtmlXPathSelector(response)
         self.conn = sqlite3.connect(self.filename)
         self.conn.text_factory = 'utf-8'
@@ -48,18 +112,21 @@ class WorldPlaceSpider(BaseSpider):
             else:
                 semi_continent = row.select('td[1]/text()').extract()[0]
                 url = ''
-            self.conn.execute('insert into semi_continent (title, level, url) values (?,?,?)', (semi_continent, 2, url))
+            print semi_continent + "  " + url
+            log.msg(semi_continent + "  " + url)
+            # self.conn.execute('insert into semi_continent (title, level, url) values (?,?,?)', (semi_continent, 2, url))
 
             # 获取国家信息并存入数据库，一些信息有错、漏，需要手动调整
             cs = row.select('td[2]/a')
             for c in cs:
                 country = c.select('text()').extract()[0]  # 国家名称
                 url = c.select('@href').extract()[0]  # 国家所引用的url
-                self.conn.execute('insert into country (title, level, parent_title, url) values (?,?,?,?)', (country, 3, semi_continent, url))
+                print "  " + country + "  " + url
+                # self.conn.execute('insert into country (title, level, parent_title, url) values (?,?,?,?)', (country, 3, semi_continent, url))
 
         if self.conn is not None:
             self.conn.commit()
             self.conn.close()
             self.conn = None
 
-        return item
+        return Item()
