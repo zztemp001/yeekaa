0e1c870a6ca6ae1f1e1d45f7009f8490cc935c03
zwm <zztemp001@gmail.com>
2012-11-13 00:26:17 +0800

    update

--- zzlib/spider/proxy.py
+++ zzlib/spider/proxy.py
@@ -2,118 +2,69 @@
 
 import re
 import sqlite3
-import urllib2
-from BeautifulSoup import BeautifulSoup
+import urllib, urllib2
+import time
+import socket
 
+from libxml2 import htmlParseDoc as htmlParser
 
-def init_db():
-    conn = sqlite3.connect('proxy.db')
-    conn.execute('create table if not exists proxy (host CHAR, port CHAR, proxy_type CHAR, user CHAR, password CHAR, checked BOOL, speed INT, catch_from CHAR, catch_time TIME)')
-    return conn
+class MyProxy():
+    def __init__(self):
+        socket.setdefaulttimeout(30)
 
-def get_cnproxy_proxy():
-    ''' 数据源:http://www.cnproxy.com/proxy1.html(proxy1.html到proxy10.html共10页)
-    更新频率:几天一次
-    访问速度:比较快
-    代理数:>1000
-    这个网站的端口信息不是明文,替换下文本就可以得到端口信息了
-    '''
-    global g_proxies
+    def get_sitedigger_proxy(self):
+        url = 'http://www.site-digger.com/html/articles/20110516/proxieslist.html'
+        xpath = '//*[@id="content_detail"]/div[3]/p[3]/textarea'
 
-    for i in range(1,11):
-        url = 'http://www.cnproxy.com/proxy%d.html' % i
-        soup = BeautifulSoup(get_html(url))
+        try:
+            html = urllib2.urlopen(url).read()
+            doc = htmlParser(html, 'utf-8')
+            doc = htmlParser(html, 'utf-8')
+            content = doc.xpathEval(xpath)[0].content
+        except:
+            pass
 
-        for table in soup.find_all('table')[2:]:
-            for tr in table.find_all('tr')[1:]:
-                tds = tr.find_all('td')
-                if len(tds) >= 2 and len(tds[0]) > 0 and len(tds[1]) > 0:
-                    if tds[1].text == 'HTTP':
-                        data = tds[0].text.encode('mbcs')
-                        data = data.replace("document.write(","")
-                        data = data.replace("\"","")
-                        data = data.replace(")","")
+        proxies = []
+        for ip_port in content.split('\n')[2:-2]:
+            host = ip_port.split(':')[0]
+            port = ip_port.split(':')[1]
+            proxies.append((host, port))
 
-                        data = data.replace("+l","9")
-                        data = data.replace("+r","8")
-                        data = data.replace("+i","7")
-                        data = data.replace("+w","6")
-                        data = data.replace("+b","5")
-                        data = data.replace("+m","4")
-                        data = data.replace("+z","3")
-                        data = data.replace("+k","2")
-                        data = data.replace("+c","1")
-                        data = data.replace("+d","0")
+        self.save_proxy(proxies)
 
-                        proxy = {}
-                        proxy['url'] = data
-                        proxy['time'] = 0
+    def save_proxy(self, proxies):
+        conn = sqlite3.connect('proxy.db')
+        conn.execute('create table if not exists proxy (host CHAR, port CHAR, proxy_type CHAR, user CHAR, password CHAR, checked BOOL, speed INT, catch_from CHAR, catch_time TIME)')
+        if proxies:
+            for host, port in proxies:
+                conn.execute('insert into proxy (host, port, proxy_type) values (?,?,?)', (host, port, 'http'))
+        conn.commit()
+        conn.close()
+        conn = None
 
-                        if not (proxy in g_proxies):
-                            g_proxies.append(proxy)
+    def verify_proxy(self):
+        url = 'http://www.myip.cn'
+        xpath = '/html/body/center/div[4]/font[1]/b'
 
-def get_51proxied_proxy():
-    ''' 数据源:无忧代理
-    http://www.51proxied.com/http_fast.html
-    http://www.51proxied.com/http_anonymous.html
-    http://www.51proxied.com/http_non_anonymous.html
-    更新频率:一天多次
-    访问速度:较慢
-    代理数:>2000
-    '''
-    urls = list()
-    urls.append('http://www.51proxied.com/http_fast.html')
-    urls.append('http://www.51proxied.com/http_anonymous.html')
-    urls.append('http://www.51proxied.com/http_non_anonymous.html')
+        conn = sqlite3.connect('proxy.db')
+        proxies = conn.execute('select host, port from proxy').fetchall()
+        proxies = proxies[21:30]
 
-    for url in urls:
-        html = get_html(url)
-        if len(html) == 0:
-            continue
-        soup = BeautifulSoup(html)
-        for div in soup.find_all('div')[9:10]:
-            for tr in div.find_all('tr')[1:]:
-                tds = tr.find_all('td')
-                if len(tds) >= 3 and len(tds[1]) > 0 and len(tds[2]) > 0:
-                    proxy_url = '%s:%s' % (tds[1].text.encode('utf8'),tds[2].text.encode('utf8'))
-                    add_proxy(proxy_url)
-
-def get_01kk_proxy():
-    ''' 数据源:http://www.01kk.net/daili/
-    更新频率:几天一次
-    访问速度:比较快
-    代理数:<90
-    '''
-    global g_proxies
-    soup = BeautifulSoup(get_html('http://www.01kk.net/daili/'))
-
-    for table in soup.find_all('table'):
-        for tr in table.find_all('tr')[1:]:
-            tds = tr.find_all('td')
-            if len(tds) >= 2 and len(tds[0]) > 0 and len(tds[0]) > 0:
-                proxy = {}
-                proxy['url'] = '%s:%s' % (tds[0].text.encode('mbcs'),tds[1].text.encode('mbcs'))
-                proxy['time'] = 0
-
-                if not (proxy in g_proxies):
-                    g_proxies.append(proxy)
-
-def  get_proxylists_proxy():
-    ''' 数据源: http://www.proxylists.net/cn_0.html 0到38,可以更多
-    访问速度:非常慢,因为页数太多了
-    代理数:<400 (有一半以上的代理跟之前几个代理页面内容重复)
-    '''
-    for i in range(0,39):
-        url = 'http://www.proxylists.net/cn_%d.html' % i
-        soup = BeautifulSoup(get_html(url))
-        for table in soup.find_all('table')[1:2]:
-            for tr in table.find_all('tr')[2:]:
-                tds = tr.find_all('td')
-                if len(tds) >= 2:
-                    script = tds[0].find_all('script')
-                    if len(script) > 0:
-                        proxy_url = '%s:%s' % (urllib.unquote(script[0].text[84:-13]),tds[1].text)
-                        add_proxy(proxy_url)
+        for host, port in proxies:
+            proxy = {'http': 'http://' + host + ':' + port}
+            print u'正在使用的代理：', host, port
+            start_time = time.time()
+            try:
+                html = urllib.urlopen(url, proxies=proxy).read()
+                doc = htmlParser(html, 'utf-8')
+                content = doc.xpathEval(xpath)[0].content
+                print u'原始数据：', content
+                ip_there = content.split(' ')[1]
+                print u'远程返回地址：', ip_there
+            except Exception, e:
+                print u'错误：', e
+            time_used = time.time() - start_time
+            print u'用时：', time_used
 
 def _get_proxylist():
     urls = []
@@ -147,4 +98,5 @@ def get_proxy_list():
     return proxies
 
 if __name__ == '__main__':
-    init_db()
+    job = MyProxy()
+    job.verify_proxy()
